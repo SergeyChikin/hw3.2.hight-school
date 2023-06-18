838e101ce17d80b64764fcfe30107001dcbf6a30
Sergey <sergeychikin1981@gmail.com>
2023-06-18 19:49:12 +0300

    First commit.

---
+++ src/main/java/ru/hogwarts/hightschool/HightSchoolApplication.java
@@ -0,0 +1,13 @@
+package ru.hogwarts.hightschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HightSchoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HightSchoolApplication.class, args);
+	}
+
+}
